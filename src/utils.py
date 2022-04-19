b81a59117df330e4b08a6c599106e97724046cbd
unknown <juan.bellon2001@hotmail.com>
2022-04-19 11:29:53 +0100

    check function added

--- src/utils.py
+++ src/utils.py
@@ -24,6 +24,18 @@ def movedown(pos, maze, ysize):
         return 1
     return 0
 
+def check(pos,maze,side,xsize,ysize):
+    if side=='L' and (maze[pos[0]][pos[1] - 1][2] != 1) and pos[1] > 1:
+        return True
+    elif side =='R' and (maze[pos[0]][pos[1] + 1][3] != 1) and pos[1] < xsize:
+        return True
+    elif side == 'U' and (maze[pos[0] - 1][pos[1]][0] != 1) and pos[0] > 1:
+        return True
+    elif side == 'D' and (maze[pos[0] + 1][pos[1]][1] != 1) and pos[0] < ysize:
+        return True
+    else:
+        return False
+
 def printMaze(maze, pos, xsize, ysize):
     
     # converte a matriz estado numa matriz para "display friendly"

2c3b1ab8c0fc27e2d37336adc01333114e4868a2
Nuno Castro <up202003324@fe.up.pt>
2022-04-20 23:25:59 +0100

    dfs done and bfs partially

--- src/ai.py
+++ src/ai.py
@@ -22,4 +22,59 @@ def addCost():
 
 def getCost():
     return valueCost
+
+def dfs(neighbours, position, destination, visited = None, path = None): 
+
+    if visited is None:
+        visited = []
+    if path is None:
+        path = []
+
+    visited.add(position)
+    path.append(position)
+
+    if position[0] != destination[0] or position[1] != destination[1]:
+
+        neighbours = checkNeighbours()
+
+        for neighbour in neighbours[position]:
+            if neighbour not in visited:
+                dfs(neighbours, neighbour, visited, path)
+
+    return path
     
+def bfs_shortest_path(graph, start, goal):
+    # keep track of explored nodes
+    explored = []
+    # keep track of all the paths to be checked
+    queue = [[start]]
+ 
+    # return path if start is goal
+    if start == goal:
+        return "That was easy! Start = goal"
+ 
+    # keeps looping until all possible paths have been checked
+    while queue:
+        # pop the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            # go through all neighbour nodes, construct a new path and
+            # push it into the queue
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                # return path if neighbour is goal
+                if neighbour == goal:
+                    return new_path
+ 
+            # mark node as explored
+            explored.append(node)
+ 
+    # in case there's no path between the 2 nodes
+    return "So sorry, but a connecting path doesn't exist :("
+ 
+bfs_shortest_path(graph, 'G', 'D') 
